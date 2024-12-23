e3cc1cfb030eb5c0d6f89999af5146bc6942f332
GiacomoAru <giacomoaru01@gmail.com>
2024-12-23 10:48:47 +0100

    new gitignore

--- testcase_1.py
+++ testcase_1.py
@@ -3,15 +3,10 @@ from plot_utils import *
 from math_utils import *
 from low_rank_approximation import *
 
-seed = 123
-seeds = [123, 456, 789, 101112]
+seeds = [123, 456]
 
-methods = [
-    'rand_n',           # Normal Distribution
-    'sketching_g',      # Sketching with Gaussian Distribution
-    'sketching_b',      # Sketching with Bernoulli Distribution
-]
-epsilon = 1e-08
+methods = 'rand_n'
+epsilon = 1e-15
 
 ks = [
     1,
@@ -21,9 +16,13 @@ ks = [
 ]
 
 
-noises = []
-A = add_gaussian_noise(np.eye(100), 0, 1e-07)
+A = np.eye(100)
+noises = [np.zeros_like(A)] + [np.random.normal(0, 10**-x, A.shape) for x in range(9)].reverse()
+
 for seed in seeds:
-    for k in ks:
-        for meth in methods:
-            start(A, k, 'diagonal', 'eighen_bad_bad_250x250',  f'{k}_{meth}_{epsilon}', meth, epsilon=epsilon, seed=seed)
+    for i, noise in enumerate(noises):
+        for k in ks:
+            for meth in methods:
+                start(A + noise, k, 'identity', f'eye100x100_{10**-i}',  
+                      f'{k}_{meth}_{seed}', meth, epsilon=epsilon, seed=seed)  
+                continue
